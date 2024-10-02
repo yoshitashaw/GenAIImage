8b32a5918a03bd6aaab57dc9e2eedb1a67854693
Yoshita Shaw <yoshitashaw@gmail.com>
2024-10-02 10:48:13 +0530

    Cloudinary Setup

--- client/vite-project/src/pages/CreatePost.jsx
+++ client/vite-project/src/pages/CreatePost.jsx
@@ -40,14 +40,41 @@ function CreatePost() {
       } finally {
         setGeneratingImg(false);
       }
-    } else {
+    } 
+    else {
       alert('Please enter a prompt');
     }
+
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Add your submission logic here
+  const handleSubmit = async(e) => {
+    e.preventDefault();  //prevents the browser from automatically reload our application
+
+    if(form.prompt  &&  form.photo){
+      setLoading(true);
+
+      try{
+        const response = await fetch('http://localhost:8080/api/v1/post',{
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(form)
+          }
+        )
+        await response.json();
+        navigate('/');
+      }
+      catch(error){
+        alert(error)
+      }
+      finally{
+        setLoading(false);
+      }
+    }
+    else {
+      alert('Please enter a prompt and generate an image!');
+    }
   };
   
   const handleChange = (e) => {
